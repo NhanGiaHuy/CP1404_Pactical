b0afb9e3bf71812384d975aff2aadfe019536023
NhanGiaHuy <30426661+NhanGiaHuy@users.noreply.github.com>
2018-11-27 10:29:34 +0800

    fixed done

--- Prac_03/temperatures.py
+++ Prac_03/temperatures.py
@@ -7,13 +7,12 @@ def main():
     celsius_temperature = Fahrenheit_to_Celsius(fahrenheit_temperature)
     print(str(round(celsius_temperature, 2)))
 
-
 def Celsius_to_Fahrenheit(celsius_temperature):
-    fahrenheit_temperature = (celsius_temperature * (9/5)) + 32
+    fahrenheit_temperature = (celsius_temperature * (9 / 5)) + 32
     return fahrenheit_temperature
 
 def Fahrenheit_to_Celsius(fahrenheit_temperature):
-    celsius_temperature = (fahrenheit_temperature - 32) * (5/9)
+    celsius_temperature = (fahrenheit_temperature - 32) * (5 / 9)
     return celsius_temperature
 
 main()
--- Prac_03/word_generator.py
+++ Prac_03/word_generator.py
@@ -1,21 +1,21 @@
-"""
-CP1404/CP5632 - Practical
-Random word generator - based on format of words
-
-Another way to get just consonants would be to use string.ascii_lowercase
-(all letters) and remove the vowels.
-"""
 import random
 
-VOWELS = "aeiou"
-CONSONANTS = "bcdfghjklmnpqrstvwxyz"
+def main():
+    VOWELS = "aeiou"
+    CONSONANTS = "bcdfghjklmnpqrstvwxyz"
 
-word_format = "ccvcvvc"
-word = ""
-for kind in word_format:
-    if kind == "c":
-        word += random.choice(CONSONANTS)
-    else:
-        word += random.choice(VOWELS)
+    word_format = "ccvcvvc"
+    word = ""
+    word = generate_word(CONSONANTS, VOWELS, word, word_format)
 
-print(word)
+    print(word)
+
+
+def generate_word(CONSONANTS, VOWELS, word, word_format):
+    for kind in word_format:
+        if kind == "c":
+            word += random.choice(CONSONANTS)
+        else:
+            word += random.choice(VOWELS)
+    return word
+
